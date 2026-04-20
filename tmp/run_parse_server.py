ff0a28cba1b55c628f82ea733e066b3db1ca4288
dimadde39-del <254264660+dimadde39-del@users.noreply.github.com>
2026-04-20 16:49:36 +0500

    chore: save local changes before merge

---
+++ tmp/run_parse_server.py
@@ -0,0 +1,7 @@
+﻿import sys
+sys.path.insert(0, r"C:\ForkVerse")
+from http.server import HTTPServer
+from api.parse import handler
+server = HTTPServer(("127.0.0.1", 8124), handler)
+print("SERVING", flush=True)
+server.serve_forever()
